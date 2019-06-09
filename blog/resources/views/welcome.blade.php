0905ae26e449334646842256e03466cc2b92ce76
sharad2307 <manuandmehrotra@gmail.com>
2019-06-09 17:20:10 +0530

    new

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 
-@section
+
 
 @section('content')
 <h1>My first website  laravell but fck</h1>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,16 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/','Pagescontroller@home');
+Route::get('/about','Pagescontroller@about');
+Route::get('/contact','Pagescontroller@contact');
 
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+
+// Route::get('/about', function () {
+//     return view('about');
+// });
+
+
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
 
